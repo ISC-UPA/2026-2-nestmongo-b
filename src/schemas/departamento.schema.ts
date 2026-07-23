b2f8208ee1614b3473c0afbd02604b1d829c8b65
carlosherrerah <carlos.herrera@upa.edu.mx>
2026-07-23 15:32:27 -0600

    Auto 2026-07-23 15:32

--- src/schemas/departamento.schema.ts
+++ src/schemas/departamento.schema.ts
@@ -1,1 +1,11 @@
-hola
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema({ collection: 'Nombre '})
+export class Nombre  extends Document {
+    @Prop({ required: true, unique: true, index: true })
+    id_!: number;
+
+}
+
+export const Nombre Schema = SchemaFactory.createForClass(Nombre );
