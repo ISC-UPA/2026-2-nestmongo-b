395393555e1ea1e9fc50352606d230c2fdb5d079
carlosherrerah <carlos.herrera@upa.edu.mx>
2026-07-23 15:46:33 -0600

    Auto 2026-07-23 15:46

--- src/services/departamentos.service.ts
+++ src/services/departamentos.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Departamento } from '../schemas/departamento.schema';
 
 @Injectable()
-export class DepartamentosService {}
+export class DepartamentosService {
+    constructor(@InjectModel(Departamento.name) private readonly departamentoModel: Model<Departamento>) { }
+
+    async findAll(): Promise<Departamento[]> {
+        return this.departamentoModel.find().sort({ id_dep: 1 }).exec();
+    }
+
+}
